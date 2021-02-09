0bd4cee26370d3205f19f8543b3af3862d64cab9
Jihyun <hwiyu25@gmail.com>
2021-02-09 23:13:22 +0900

    🔧 미들웨어 추가

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 import express from "express";
 
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import morgan from "morgan";
+
 const app = express();
+
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+  })
+);
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(morgan(dev));
 
 export default app;
