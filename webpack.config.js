99d68d64d1c91efc87c088e5a777669a6e382997
Jihyun <hwiyu25@gmail.com>
2021-02-15 21:35:08 +0900

    🐛 webpack 오류 수정

--- webpack.config.js
+++ webpack.config.js
@@ -2,35 +2,60 @@ const path = require("path");
 const autoprefixer = require("autoprefixer");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-const ENTRY = path.resolve(__dirname, assets, js, main.js);
+const MODE = process.env.WEBPACK_ENV;
+const ENTRY_FILE = path.resolve(__dirname, "assets", "js", "main.js");
 const OUTPUT_DIR = path.join(__dirname, "static");
 
-const MODE = process.env.WEBPACK_ENV;
-
-module.exports = {
-  devtool: "cheap-module-source-map",
-  entry: ENTRY,
+const config = {
+  entry: ENTRY_FILE,
   mode: MODE,
-  output: {
-    filename: "[name].js",
-    path: OUTPUT_DIR,
-  },
   module: {
     rules: [
       {
         test: /\.(js)$/,
-        use: ["babel-loader"],
+        use: [
+          {
+            loader: "babel-loader",
+          },
+        ],
       },
       {
         test: /\.(scss)$/,
         use: [
-          MiniCssExtractPlugin.loader,
-          "css-loader",
-          "postcss-loader",
-          "sass-loader",
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: "css-loader", //webpack이 CSS를 이해하도록 가르쳐줌
+          },
+          {
+            loader: "postcss-loader", //호환성을 위한 것(번역만이 목적이 아님)
+            options: {
+              postcssOptions: {
+                plugins() {
+                  return [autoprefixer({ browsers: "cover 99.5%" })];
+                },
+              },
+            },
+          },
+          {
+            loader: "sass-loader", //sass, scss를 이해하기 위함
+          },
         ],
       },
     ],
   },
-  plugin: [new MiniCssExtractPlugin({ filename: "style.css" })],
+  output: {
+    path: OUTPUT_DIR,
+    filename: "[name].js",
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: "style.css",
+    }),
+  ],
 };
+
+module.exports = config;
