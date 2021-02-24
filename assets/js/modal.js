224c871501e558c34524db9bce78a5830d2a9222
Jihyun <hwiyu25@gmail.com>
2021-02-24 10:31:10 +0900

    ✨ contentDetail 모달 추가

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,6 +1,7 @@
 const modalBox = document.querySelectorAll(".jsModalBox");
 const ellipsis = document.querySelectorAll(".jsModalShow");
 const closeBtn = document.querySelectorAll(".jsModalClose");
+const contentDetailBtn = document.querySelector(".jsContentDetailBtn");
 
 const handleEllipsisClick = (e) => {
   const modal = e.target.parentNode.parentNode.parentNode.lastChild;
@@ -12,6 +13,12 @@ const handlecloseBtn = (e) => {
   modal.classList.add("modal-hidden");
 };
 
+const showContentDetailModal = (e) => {
+  const modal =
+    e.target.parentNode.parentNode.parentNode.parentNode.nextSibling;
+  modal.classList.remove("modal-hidden");
+};
+
 const init = () => {
   ellipsis.forEach((btn) => {
     btn.addEventListener("click", handleEllipsisClick);
@@ -19,6 +26,9 @@ const init = () => {
   closeBtn.forEach((btn) => {
     btn.addEventListener("click", handlecloseBtn);
   });
+  if (contentDetailBtn) {
+    contentDetailBtn.addEventListener("click", showContentDetailModal);
+  }
 };
 
 if (modalBox) {
