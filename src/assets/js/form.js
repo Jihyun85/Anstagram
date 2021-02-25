962fd1597f808696ffdc3f41437746195a9b50a2
Jihyun <hwiyu25@gmail.com>
2021-02-25 14:08:25 +0900

    ✨ 파일 업로드가 확인되면 '선택 완료'로 버튼 바뀌는 기능 추가

---
+++ src/assets/js/form.js
@@ -0,0 +1,34 @@
+const fileInput = document.querySelector(".jsFileInput");
+const profileInput = document.querySelector(".jsProfileInput");
+const fileLabel = document.querySelector(".jsFileLabel");
+
+const chooseFile = (e) => {
+  const file = e.target.files.length;
+  if (file === 1) {
+    fileLabel.innerText = "이미지 선택 완료";
+  } else if (file === 0) {
+    fileLabel.style.backgroundColor = "";
+    fileLabel.innerText = "이미지 선택하기";
+  }
+};
+
+const chooseProfile = (e) => {
+  const file = e.target.files.length;
+  if (file === 1) {
+    fileLabel.innerText = "프로필 사진 선택 완료";
+  } else if (file === 0) {
+    fileLabel.innerText = "프로필 사진 선택";
+  }
+};
+
+const init = () => {
+  if (fileInput) {
+    fileInput.addEventListener("change", chooseFile);
+  } else if (profileInput) {
+    profileInput.addEventListener("change", chooseProfile);
+  }
+};
+
+if (fileLabel) {
+  init();
+}
--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -4,3 +4,4 @@ import "./addComment.js";
 import "./modal.js";
 import "./header.js";
 import "./textarea.js";
+import "./form.js";
